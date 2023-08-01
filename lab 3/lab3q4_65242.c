5cac1b9a2d48d51aabd86918b959fc1dde4ba37d
borball12 <ball.pornnapat@gmail.com>
2023-08-01 15:53:49 +0700

    update lab4

--- lab 3/lab3q4_65242.c
+++ lab 3/lab3q4_65242.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 int main() {
     int count = 0;
-    int total = 0;
+    int sum = 0;
 
     while(1) {
         int a;
         printf("Enter a positive integer : ");
         scanf("%d", &a);
+        count += 1;
+        sum += a;
         if (a <= 0) break;
-        count += a;
-        total += 1;
+        
     }
     
-    printf("addition = %d average = %d\n", count, count / total);
+    printf("addition = %d average = %d\n", sum, sum / count,);
     return 0;
 }
